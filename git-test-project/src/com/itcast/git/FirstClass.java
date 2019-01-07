9aa9fb70d260be5ad6a056ef107ff9fd9747366b
xia <xia@.cn>
2019-01-07 20:19:01 +0800

    添加本地工程

---
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+    }
+}
